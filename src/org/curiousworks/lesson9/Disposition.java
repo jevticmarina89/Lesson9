7c2de58d3b85017f9b7deefcb448a1405338a66d
jevticmarina89 <marina.jevtic89@gmail.com>
2019-06-15 16:21:50 +0200

    Dragons in love, lions in fight,

---
+++ src/org/curiousworks/lesson9/Disposition.java
@@ -0,0 +1,21 @@
+package org.curiousworks.lesson9;
+
+public class Disposition {
+	enum Feeling {happy, sad, love}
+	
+	
+	static String[] happyEmoji = {"😄", "🙂", "😊", "😍"};
+	static String[] sadEmoji = {"😢", "😭", "😞", "👿"};
+	static String[] loveEmoji = {"😍", "😘"};
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+
+}
